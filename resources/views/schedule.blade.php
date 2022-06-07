e44ce0a6dcacb0615a68a0bfa509389ab11b8c7e
Darwish Mat Zain <darwish@darwishzain.com>
2022-06-07 12:36:41 +0800

    : ]

---
+++ resources/views/schedule.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Schedule</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+    <div class="container-fluids">
+        <div class="row title">
+            <h1>Schedule</h1>
+        </div>
+        <div class="row">
+            <div class="col-lg-4">
+                <table class="table">
+                    
+                </table>
+            </div>
+        </div>
+    </div>
+    <script src="/js/app.js"></script>
+</body>
+</html>
